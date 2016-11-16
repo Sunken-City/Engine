93aa9d56eeb4667d869f8d4f18a9c8284317e9bd
Anthony Cloudy <acloudy@mail.smu.edu>
2016-11-16 14:56:04 -0600

    Updated the header for named properties.

--- Code/Engine/Core/Events/NamedProperties.hpp
+++ Code/Engine/Core/Events/NamedProperties.hpp
@@ -5,10 +5,20 @@
 enum PropertyGetResult
 {
     PGR_SUCCESS,
-    PGR_WRONG_TYPE,
-    PGR_NOT_PRESENT,
-    PGR_EMPTY,
-    NUM_TYPES
+    PGR_FAILED_WRONG_TYPE,
+    PGR_FAILED_NOT_PRESENT,
+    PGR_FAILED_EMPTY,
+    NUM_RESULTS
+};
+
+//-----------------------------------------------------------------------------------
+enum PropertySetResult
+{
+    PSR_SUCCESS,
+    PSR_SUCCESS_EXISTED,
+    PSR_SUCCESS_CHANGED_TYPE,
+    PSR_FAILED_DIFF_TYPE,
+    NUM_RESULTS
 };
 
 //-----------------------------------------------------------------------------------
@@ -19,4 +29,12 @@ class NamedProperties
     {
 
     };
+
+    template<typename T>
+    PropertySetResult Set(const std::string& propertyName, const T& propertyValue, bool changeTypeIfDifferent = true)
+    {
+
+    };
+
+    void Remove(const std::string& propertyName);
 };
