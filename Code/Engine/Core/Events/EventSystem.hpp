f40bf571baadae696ca4584973cd733246b78b0d
Anthony Cloudy <acloudy@mail.smu.edu>
2016-11-16 16:25:36 -0600

    Added some more declarations

--- Code/Engine/Core/Events/EventSystem.hpp
+++ Code/Engine/Core/Events/EventSystem.hpp
@@ -1,6 +1,21 @@
 #pragma once
+#include "Engine/Core/Events/NamedProperties.hpp"
+#include <map>
+#include <string>
+#include <vector>
 
+typedef void (EventCallbackFunction)(NamedProperties& params);
+
+//-----------------------------------------------------------------------------------
 class EventSystem
 {
 
+    //CONSTRUCTORS/////////////////////////////////////////////////////////////////////
+
+    //FUNCTIONS/////////////////////////////////////////////////////////////////////
+    void RegisterEventCallback(const std::string& name, EventCallbackFunction function);
+    void FireEvent(const std::string& name);
+
+    //MEMBER VARIABLES/////////////////////////////////////////////////////////////////////
+    std::map<std::string, std::vector<EventCallbackFunction>> m_registeredFunctions;
 };
