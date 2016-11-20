5baf298be401b2b318a4e57e9c0d71e2535771de
Anthony Cloudy <acloudy@mail.smu.edu>
2016-11-20 01:08:25 -0600

    Implemented function and method event callbacks!

--- Code/Engine/Core/Events/EventSystem.cpp
+++ Code/Engine/Core/Events/EventSystem.cpp
@@ -1,1 +1,27 @@
 #include "Engine/Core/Events/EventSystem.hpp"
+
+
+std::map<std::string, std::vector<RegisteredObjectBase*>> EventSystem::s_registeredFunctions;
+
+//-----------------------------------------------------------------------------------
+void FireEvent(const char* name, NamedProperties& namedProperties)
+{
+    std::vector<RegisteredObjectBase*> functions = EventSystem::s_registeredFunctions[std::string(name)];
+    for (RegisteredObjectBase* callee : functions)
+    {
+        callee->Execute(namedProperties);
+    }
+}
+
+//-----------------------------------------------------------------------------------
+void FireEvent(const std::string& name)
+{
+
+}
+
+//-----------------------------------------------------------------------------------
+void EventSystem::RegisterEventCallback(const std::string& eventName, EventCallbackFunction* m_function)
+{
+    RegisteredFunction* rom = new RegisteredFunction(m_function);
+    s_registeredFunctions[eventName].push_back(rom);
+}
--- Code/Engine/Core/Events/NamedProperties.cpp
+++ Code/Engine/Core/Events/NamedProperties.cpp
@@ -1,1 +1,53 @@
 #include "Engine/Core/Events/NamedProperties.hpp"
+
+//-----------------------------------------------------------------------------------
+NamedProperties::NamedProperties()
+{
+
+}
+
+//-----------------------------------------------------------------------------------
+NamedProperties::~NamedProperties()
+{
+    for (auto keyValuePair : m_properties)
+    {
+        delete keyValuePair.second;
+    }
+    m_properties.clear();
+}
+
+//-----------------------------------------------------------------------------------
+PropertySetResult NamedProperties::Set(const std::string& propertyName, const char* propertyValue, bool changeTypeIfDifferent)
+{
+    PropertySetResult result = PSR_SUCCESS;
+    if (m_properties.find(propertyName) != m_properties.end())
+    {
+        if (changeTypeIfDifferent)
+        {
+            result = PSR_SUCCESS_EXISTED;
+        }
+        else
+        {
+            result = PSR_FAILED_DIFF_TYPE;
+            return result;
+        }
+    }
+    m_properties[propertyName] = static_cast<NamedPropertyBase*>(new TypedNameProperty<const char*>(propertyValue));
+    return result;
+}
+
+//-----------------------------------------------------------------------------------
+bool NamedProperties::Remove(const std::string& propertyName)
+{
+    auto foundPair = m_properties.find(propertyName);
+    if (foundPair != m_properties.end())
+    {
+        delete foundPair->second;
+        m_properties.erase(foundPair);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
