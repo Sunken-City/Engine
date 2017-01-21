da71685faae6415f5b71d79928bcbe852ed5159e
Anthony Cloudy <acloudy@mail.smu.edu>
2017-01-21 17:41:16 -0600

    Added a font size as a stopgap measure until text rendering is overhauled

--- Code/Engine/Renderer/2D/TextRenderable2D.cpp
+++ Code/Engine/Renderer/2D/TextRenderable2D.cpp
@@ -39,9 +39,9 @@ void TextRenderable2D::Render(BufferedMeshRenderer& renderer)
     Matrix4x4::MatrixMakeTranslation(&translation, Vector3(m_transform.GetWorldPosition(), 0.0f));
     renderer.SetModelMatrix(scale * rotation * translation);
 
-    AABB2 bounds = m_font->CalcTextBounds(m_text, TEXT_SANITY_CONSTANT);
+    AABB2 bounds = m_font->CalcTextBounds(m_text, TEXT_SANITY_CONSTANT * m_fontSize);
     Vector2 size = Vector2(bounds.GetWidth(), bounds.GetHeight());
-    renderer.m_builder.AddText2D(size * -0.5f, m_text, TEXT_SANITY_CONSTANT, RGBA::WHITE, true, m_font);
+    renderer.m_builder.AddText2D(size * -0.5f, m_text, TEXT_SANITY_CONSTANT * m_fontSize, m_color, true, m_font);
     renderer.m_builder.CopyToMesh(&renderer.m_mesh, &Vertex_Sprite::Copy, sizeof(Vertex_Sprite), &Vertex_Sprite::BindMeshToVAO);
 
 #pragma todo("This should be unneccessary once we have batching done properly")
--- Code/Engine/Renderer/2D/TextRenderable2D.hpp
+++ Code/Engine/Renderer/2D/TextRenderable2D.hpp
@@ -2,6 +2,7 @@
 #include "Renderable2D.hpp"
 #include <string>
 #include "Engine\Math\Transform2D.hpp"
+#include "..\RGBA.hpp"
 
 class BufferedMeshRenderer;
 class AABB2;
@@ -24,4 +25,6 @@ public:
     std::string m_text;
     Transform2D m_transform;
     BitmapFont* m_font;
+    float m_fontSize = 1.0f;
+    RGBA m_color = RGBA::WHITE;
 };
